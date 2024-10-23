7efa5cf945c3e7d5c7159e9a97014fe77c67d2a2
Ashish Giri Goswami <148761441+ashishji001@users.noreply.github.com>
2024-10-23 22:19:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const themeToggler = document.querySelector(".theme-toggler");
+
+
+
+themeToggler.onclick = function() {
+    document.body.classList.toggle('dark-theme');
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active')
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active')
+}
+
